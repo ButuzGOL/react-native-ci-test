baa8a00c6f6f640a603700e06380b1327f8038d9
ButuzGOL <ButuzGOL@gmail.com>
2018-05-07 11:46:12 +0300

    testt

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,7 +6,9 @@ changes() {
 
 changed=$(changes | grep -c "htmobile-ui")
 
-if [ $changed -eq 0 ]; then
-  echo "No folder"
-  exit 1
-fi
+curl -i -X PATCH -H "X-API-Token:bb6f887a-bd87-46a8-81f1-466a7d66589d" -H "Content-Type: application/json" -d "{\"status\":\"cancelling\"}" https://appcenter.ms/api/v0.1/apps/ButuzGOL/rn-ci-test-android/builds/$APPCENTER_BUILD_ID 
+
+# if [ $changed -eq 0 ]; then
+#   echo "No folder"
+#   exit 1
+# fi
