f5d4b0c55fd906112358b9aa7c3335754b368ca5
ButuzGOL <ButuzGOL@gmail.com>
2018-05-07 11:54:45 +0300

    Postgit push

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -6,7 +6,7 @@ changes() {
 
 changed=$(changes | grep -c "htmobile-ui")
 echo "TTTTTTTTTTTT"
-echo $APPCENTER_REACTNATIVE_PACKAGE
+echo $APPCENTER_XCODE_PROJECT
 # curl -i -X PATCH -H "X-API-Token:ece7b10ec8e809cc45c29823538cb85601cda5cf" -H "Content-Type: application/json" -d "{\"status\":\"cancelling\"}" https://appcenter.ms/api/v0.1/apps/ButuzGOL/rn-ci-test-android/builds/$APPCENTER_BUILD_ID 
 
 # if [ $changed -eq 0 ]; then
